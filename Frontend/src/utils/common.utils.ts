1c910eed6d52b0959e3075c3f07d4c03ecc20ae9
Aquarius2301 <khangta67@gmail.com>
2026-09-16 12:16:10 +0700

    fix getVisitorId

--- Frontend/src/utils/common.utils.ts
+++ Frontend/src/utils/common.utils.ts
@@ -1,19 +1,18 @@
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
 
-let cached: string | null = null;
-
+let cachedPromise: Promise<string> | null = null;
 /**
  * Returns the cached FingerprintJS visitor id, loading it once on first call.
  * The visitor id is sent to the backend as the `X-Device-Id` header so refresh
  * tokens can be bound to the originating device (see backend TH2 handling).
  */
 export const getVisitorId = async (): Promise<string> => {
-  if (cached) return cached;
-
-  const fp = await FingerprintJS.load();
-  cached = (await fp.get()).visitorId;
-
-  return cached;
+  if (!cachedPromise) {
+    cachedPromise = FingerprintJS.load()
+      .then((fp) => fp.get())
+      .then((r) => r.visitorId);
+  }
+  return cachedPromise;
 };
 
 /**
